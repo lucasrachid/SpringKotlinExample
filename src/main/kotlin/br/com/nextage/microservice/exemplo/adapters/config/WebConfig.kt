44deaaff4618fff8e1fa50c60c6f9a804c23fdf4
Lucas Rachid Martins <65245074+lucasrachid@users.noreply.github.com>
2022-12-23 15:03:46 -0300

    Teste

--- src/main/kotlin/br/com/nextage/microservice/exemplo/adapters/config/WebConfig.kt
+++ src/main/kotlin/br/com/nextage/microservice/exemplo/adapters/config/WebConfig.kt
@@ -6,16 +6,19 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.ClientCodecConfigurer
 import org.springframework.http.converter.HttpMessageConverter
 import org.springframework.http.converter.ResourceHttpMessageConverter
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.reactive.function.client.ExchangeStrategies
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
 import java.text.SimpleDateFormat
 import java.time.format.DateTimeFormatter
+
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
@@ -45,5 +48,18 @@ class WebConfig : WebMvcConfigurer {
     override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
         converters.add(MappingJackson2HttpMessageConverter(objectMapper()))
         converters.add(ResourceHttpMessageConverter())
+    }
+
+    @Bean
+    fun webClient(): WebClient? {
+        val size = 16 * 1024 * 1024;
+        val strategies = ExchangeStrategies.builder()
+            .codecs { codecs: ClientCodecConfigurer ->
+                codecs.defaultCodecs().maxInMemorySize(size)
+            }
+            .build()
+        return WebClient.builder()
+            .exchangeStrategies(strategies)
+            .build()
     }
 }
